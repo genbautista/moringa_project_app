372ce6061de6b7c9a9b93447cc50ea2b3ccfd801
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-16 19:11:42 +0700

    new page as well

---
+++ lib/shop.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final products = [
+      {'name': 'Facial Oil', 'price': '\$29'},
+      {'name': 'Hair Mist', 'price': '\$19'},
+      {'name': 'Body Balm', 'price': '\$24'},
+    ];
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAF7F0),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF47734E),
+        title: const Text(
+          'Shop',
+          style: TextStyle(
+            fontFamily: 'GlacialIndifference',
+            color: Color(0xFFFAF7F0),
+          ),
+        ),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: products.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, mainAxisSpacing: 16, crossAxisSpacing: 16,
+        ),
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: const Color(0xFFE8E5DC),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.spa, size: 40, color: Color(0xFF47734E)),
+                const SizedBox(height: 8),
+                Text(product['name']!,
+                    style: const TextStyle(
+                      fontFamily: 'GlacialIndifference',
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text(product['price']!,
+                    style: const TextStyle(
+                      fontFamily: 'GlacialIndifference',
+                      color: Colors.grey,
+                    )),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
