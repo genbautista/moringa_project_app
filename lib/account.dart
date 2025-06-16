dee140b307d7f63db3aa9d4ac0c47b24a37a4492
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-16 19:10:07 +0700

    created page

---
+++ lib/account.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatelessWidget {
+  const AccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAF7F0),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF47734E),
+        title: const Text(
+          'Account',
+          style: TextStyle(
+            fontFamily: 'GlacialIndifference',
+            color: Color(0xFFFAF7F0),
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Account page coming soon!',
+          style: TextStyle(
+            fontSize: 18,
+            fontFamily: 'GlacialIndifference',
+            color: Color(0xFF3A1A14),
+          ),
+        ),
+      ),
+    );
+  }
+}
