e5db3dd4bbd9ee5150f3a60bc0ba5cec05096b0c
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-19 10:30:56 +0700

    added few lines of code

---
+++ lib/explorebenefits.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ExploreBenefits extends StatefulWidget {
+  const ExploreBenefits({super.key});
+
+  @override
+  State<ExploreBenefits> createState() => _ExploreBenefitsState();
+}
+
+class _ExploreBenefitsState extends State<ExploreBenefits> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
