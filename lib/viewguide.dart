b4e6e33e38f74c7e06647854313a3f4daf9496e9
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-19 12:12:58 +0700

    route and app bar implemented

--- lib/viewguide.dart
+++ lib/viewguide.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
 class ViewGuide extends StatefulWidget {
   const ViewGuide({super.key});
@@ -11,6 +12,18 @@ class _ViewGuideState extends State<ViewGuide> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAF7F0),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF47734E),
+        title: const Text(
+          'View Guide',
+          style: TextStyle(
+            fontFamily: 'GlacialIndifference',
+            color: Color(0xFFFAF7F0),
+          ),
+        ),
+      ),
+    );
   }
 }
