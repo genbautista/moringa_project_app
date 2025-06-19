bf94e795b934943f1d770e800abb0a16e7485eac
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-19 10:34:40 +0700

    added few lines of code

---
+++ lib/viewguide.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ViewGuide extends StatefulWidget {
+  const ViewGuide({super.key});
+
+  @override
+  State<ViewGuide> createState() => _ViewGuideState();
+}
+
+class _ViewGuideState extends State<ViewGuide> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
