7231f66e9119124a6ba90a8ea3643838c640e6c6
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-19 12:12:41 +0700

    route and app bar implemented

--- lib/shopnow.dart
+++ lib/shopnow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
 class ShopNow extends StatefulWidget {
   const ShopNow({super.key});
@@ -11,6 +12,18 @@ class _ShopNowState extends State<ShopNow> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAF7F0),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF47734E),
+        title: const Text(
+          'Shop Now',
+          style: TextStyle(
+            fontFamily: 'GlacialIndifference',
+            color: Color(0xFFFAF7F0),
+          ),
+        ),
+      ),
+    );
   }
 }
