e69b6bdcdb105dbfc957f38ba70d476e93309b5a
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-19 10:31:44 +0700

    added few lines of code

---
+++ lib/learnmore.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LearnMore extends StatefulWidget {
+  const LearnMore({super.key});
+
+  @override
+  State<LearnMore> createState() => _LearnMoreState();
+}
+
+class _LearnMoreState extends State<LearnMore> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
