707f36c7f73cc328e4b11689ed438e2036770dc2
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-19 12:12:19 +0700

    route and app bar implemented

--- lib/learnmore.dart
+++ lib/learnmore.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
 class LearnMore extends StatefulWidget {
   const LearnMore({super.key});
@@ -11,6 +12,23 @@ class _LearnMoreState extends State<LearnMore> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAF7F0),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF47734E),
+        title: const Text(
+          'Learn More',
+          style: TextStyle(
+            fontFamily: 'GlacialIndifference',
+            color: Color(0xFFFAF7F0),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          
+        )
+      )
+    );
   }
 }
