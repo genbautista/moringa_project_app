dcd237e4a8c9965992c34fcc648fbb31bcdc776f
genbautista <genesis.bautistasanc@mymail.champlain.edu>
2025-06-19 10:32:26 +0700

    added few lines of code

---
+++ lib/shopnow.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ShopNow extends StatefulWidget {
+  const ShopNow({super.key});
+
+  @override
+  State<ShopNow> createState() => _ShopNowState();
+}
+
+class _ShopNowState extends State<ShopNow> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
